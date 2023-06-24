112a2886653735748bbfcdd2c3a9a582d7957bdb
Jeremy <jeremy.cavallo@wright.edu>
2023-06-24 14:25:22 -0400

    Initial commit

---
+++ train.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+git clone https://github.com/huggingface/diffusers.git
+pip install -r requirements.txt
+
+
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export TRAIN_DIR="data/train"
+export OUTPUT_DIR="generativeAAC"
+
+accelerate launch diffusers/examples/text_to_image/train_text_to_image.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --train_data_dir=$TRAIN_DIR \
+  --use_ema \
+  --resolution=512 --center_crop --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --gradient_checkpointing \
+  --mixed_precision="no" \
+  --max_train_steps=15000 \
+	--caption_column="additional_feature" \
+  --learning_rate=1e-05 \
+  --max_grad_norm=1 \
+  --lr_scheduler="constant"  \
+  --output_dir=${OUTPUT_DIR}
