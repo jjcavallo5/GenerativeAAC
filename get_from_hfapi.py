7e3eb137c10d58b5794d7903ba8a0c5d268d7a75
Jeremy Cavallo <jjcavallo@wayne.edu>
2023-06-27 09:04:27 -0400

    Added script to generate image from API

---
+++ get_from_hfapi.py
@@ -0,0 +1,54 @@
+import argparse
+import config
+import io
+from PIL import Image
+import requests
+
+#### ARGS ####
+parser = argparse.ArgumentParser()
+parser.add_argument(
+	'-p',
+	'--prompt',
+	required=True,
+	type=str,
+    help="Prompt for image generation"
+)
+parser.add_argument(
+    '-n',
+    '--name',
+    type=str,
+    default='output',
+    help="Name of output file"
+)
+args = parser.parse_args()
+
+#### API ####
+
+API_URL = "https://api-inference.huggingface.co/models/jjcavallo5/generative_aac"
+headers = {"Authorization": f"Bearer {config.HF_TOKEN}"}
+
+def query(prompt):
+
+    payload = {
+        "inputs": prompt,
+    }
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+    if response.status_code == 503:
+        print("Waiting for model to load...")
+        payload = {
+            "inputs": prompt,
+            "parameters": {
+                "wait_for_model": True
+            }
+        }
+        response = requests.post(API_URL, headers=headers, json=payload)
+
+
+    return response.content
+
+
+image_bytes = query(args.prompt)
+
+image = Image.open(io.BytesIO(image_bytes))
+image.save(f'{args.name}.png')
