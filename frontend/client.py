b3c0f04e4b7ff056c3ff596eb5b01b65f3ed18f7
Jeremy Cavallo <jjcavallo@wayne.edu>
2023-06-28 15:09:55 -0400

    Changed to create-react-app+firebase stack

--- frontend/client.py
+++
@@ -1,9 +0,0 @@
-import requests
-from PIL import Image
-from io import BytesIO
-
-HEADERS = {'Content-Type': 'application/json'}
-PAYLOAD = {"prompt": "aac symbol of a dog"}
-response = requests.post('http://127.0.0.1:8080/', headers=HEADERS, json=PAYLOAD)
-img = Image.open(BytesIO(response.content))
-img.show()
